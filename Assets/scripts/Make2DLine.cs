c1638d76a6bd6d42c9f5c26977321f374cb48200
renketsu0 <joshmarshall21@gmail.com>
2015-10-23 19:16:51 -0400

    added a half-implementation of a ui graph
    to accomplish this, I added set of open source ui extensions and a
    script that sets points. still haven't figured out how to increase the
    number of points on the line

---
+++ Assets/scripts/Make2DLine.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI.Extensions;
+using System.Collections;
+
+public class Make2DLine : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		UILineRenderer lineRenderer = GetComponent<UILineRenderer> ();
+		lineRenderer.Points [0] = new Vector2 (5, 10);
+		lineRenderer.Points [1] = new Vector2 (6, 5);
+		lineRenderer.Points [2] = new Vector2 (7,12);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
