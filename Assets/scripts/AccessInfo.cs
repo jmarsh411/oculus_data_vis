48e06f13110baa44e26c04137a0004bed9e93c68
renketsu0 <joshmarshall21@gmail.com>
2015-10-25 17:56:40 -0400

    UI string that informs which line user is on

---
+++ Assets/scripts/AccessInfo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AccessInfo : MonoBehaviour {
+	GameObject cart;
+	CreateLine lineObj;
+	Text candidateText;
+	string candPrepend = "Following: ";
+	
+	// Use this for initialization
+	void Start () {
+		cart = GameObject.Find ("Cart");
+		lineObj = cart.GetComponent<CreateLine> ();
+		candidateText = GameObject.FindGameObjectWithTag ("currCandidate").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = cart.GetComponent<Transform> ().position;
+		float x = position.x;
+		if (x == 0) {
+			candidateText.text = candPrepend + lineObj.Position2Candidate;
+		}
+		else if (x == 5) {
+			candidateText.text = candPrepend + lineObj.Position1Candidate;
+		}
+		else if (x == 10) {
+			candidateText.text = candPrepend + lineObj.Position3Candidate;
+		}
+	}
+}
