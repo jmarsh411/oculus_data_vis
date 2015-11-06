d7d1d9f299a942a732834afeb2cea5e973dde08c
renketsu0 <joshmarshall21@gmail.com>
2015-11-05 19:51:23 -0500

    added dev scene switching using l/r mouse buttons
    also smoothed a few menu details

---
+++ Assets/LoadOnClick.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+	public void LoadScene(int scene_index){
+		Application.LoadLevel (scene_index);
+	}
+
+	void Update(){
+		if (Input.GetMouseButtonDown (0)) {
+			LoadScene(1);
+		}
+		else if (Input.GetMouseButtonDown (1)) {
+			LoadScene(2);
+		}
+	}
+}
