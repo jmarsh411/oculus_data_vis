ad6bbcd560ad571ac81352f6c521aab40af35aa4
justis <justisgill@gmail.com>
2015-11-10 16:12:59 -0500

    Added Data Merging
    Adds working data merging, full map support, still no coaster updates.
    Odd bug: percent sometimes ends up greater than 100%, need to fix.

--- Assets/CSVReader.cs
+++ Assets/CSVReader.cs
@@ -1,5 +1,10 @@
 //Usage: Put CSV file in a text asset named csvFile, attach this to an object named CSVReader, 
 //Format: date, number of voters, candidate 1 name, candidate 1 percent, ... , candidate 9 name, candidate 9 percent
+//Data is read in to string rawData. It is then accessed by makeArray to make it into rawGrid. intermediaryGenerator
+//takes this raw set and removes all unused information, handling merging as well, to build intermerdiaryCoaster and
+//intermediaryMap. buildCandidateStructure then takes the processed data and builds the complete usable data in
+//proper format for both visualizations: pollByDate for map, including states, and pollByDate for coaster, excluding
+//states.
 using UnityEngine;
 using System;
 using System.Linq;
@@ -9,10 +14,13 @@ public class CSVReader : MonoBehaviour
 {
 	public static string[,] rawGrid;
 	public static string[,] intermediarySet;
+	public static string[,] intermediaryCoaster;
+	public static string[,] intermediaryMap;
     public static PollObject[,] candidateSet;
     public static DateTime[] datesRepresented;
 	public static List<Candidate> candidates = new List<Candidate>();
-	public static Dictionary<DateTime, Poll> pollByDate = new Dictionary<DateTime, Poll>();
+	public static Dictionary<DateTime, List<Poll>> pollByDate = new Dictionary<DateTime, List<Poll>>();
+	public static Dictionary<DateTime, Poll> pollByDateCoaster = new Dictionary<DateTime, Poll>();
 	public static string rawData;
 	public static int lines;
 	public static int categories;
@@ -22,8 +30,9 @@ public class CSVReader : MonoBehaviour
         TextAsset csvFile = Resources.Load("statesData") as TextAsset;
         rawData = csvFile.text;
 	    makeArray();
+		intermediaryGenerator();
         buildCandidateStructure();
-        printPolls();
+        //printPolls();
 	}
 	//Reads data in from CSV file, returns array of raw data, where each row is a line and each column is a category.
 	static void makeArray()
@@ -66,7 +75,6 @@ public class CSVReader : MonoBehaviour
 	//Currently relevant data: 1. State 2. End Date 3. Number of Voters 4. Candidate 1 5. Candidate 1 Percent ... N-1. Candidate N N. Candidate N Percent
 	static void intermediaryGenerator()
 	{
-		string[,] daySort;
 		string[] dates = new string[lines];
 		int cmx = categories - 8;
 		for (int i = 0; i < lines-1; i++) {
@@ -74,9 +82,8 @@ public class CSVReader : MonoBehaviour
 		}
 		intermediarySet = new string[lines - 1, cmx];
 		Array.Sort(dates);
-		daySort = new string[lines - 1, cmx];
-		List<string> ignore = new List<string>();
-		//remove ignore - move functionality for date merge
+		List<int> ignore = new List<int>();
+		//Arranges intermediarySet by sorted date.
 		for (int i = 0; i < lines - 1; i++)
 		{
 			for (int j = 0; j < lines - 1; j++)
@@ -85,7 +92,7 @@ public class CSVReader : MonoBehaviour
 				{
 					if (dates[i].Equals(rawGrid[j, 2]))
 					{
-						if (!ignore.Contains(rawGrid[j, 2]))
+						if (!ignore.Contains(j))
 						{
 							//date, state, numvoters, c, p, c, p ... c, p
 							intermediarySet[i, 0] = rawGrid[j, 2];
@@ -94,87 +101,158 @@ public class CSVReader : MonoBehaviour
 							{
                                 intermediarySet[i, k] = rawGrid[j, k + 8];
 							}
-							ignore.Add(rawGrid[j, 2]);
+							ignore.Add(j);
 							break;
 						}
 					}
 				}
 			}
 		}
-	}
-    static void buildCandidateStructure()
-    {
-		intermediaryGenerator();
-        Dictionary<string, int> nameOccurrences = new Dictionary<string, int>();
-        Dictionary<string, int> dateOccurrences = new Dictionary<string, int>();
-
-        int numAttributes = (intermediarySet.GetUpperBound(1) - 1) / 2;
-        for (int i = 0; i <= intermediarySet.GetUpperBound(0); i++)
-        {
-            //build date occurrence dictionary
-            string date = intermediarySet[i, 0];
-            if (date != null)
-            {
-                if (dateOccurrences.ContainsKey(date))
-                {
-                    dateOccurrences[date] += 1;
-                }
-                else
-                {
-                    dateOccurrences.Add(date, 1);
-                }
-            }
-            //build name occurrence dictionary
-            for (int j = 0; j < numAttributes; j++)
-            {
-                string key = intermediarySet[i, (j + j + 3)];
-                if (key != null)
-                {
-                    if (nameOccurrences.ContainsKey(key))
-                    {
-                        nameOccurrences[key] += 1;
-                    }
-                    else
-                    {
-                        nameOccurrences.Add(key, 1);
-                    }
-                }
-            }
-        }
+		Dictionary<string, int> nameOccurrences = new Dictionary<string, int>();
+		Dictionary<string, int> dateOccurrences = new Dictionary<string, int>();
+		
+		int numAttributes = (intermediarySet.GetUpperBound(1) - 1) / 2;
+		for (int i = 0; i <= intermediarySet.GetUpperBound(0); i++)
+		{
+			//build date occurrence dictionary
+			string date = intermediarySet[i, 0];
+			if (date != null)
+			{
+				if (dateOccurrences.ContainsKey(date))
+				{
+					dateOccurrences[date] += 1;
+				}
+				else
+				{
+					dateOccurrences.Add(date, 1);
+				}
+			}
+			//build name occurrence dictionary
+			for (int j = 0; j < numAttributes; j++)
+			{
+				string key = intermediarySet[i, (j + j + 3)];
+				if (key != null)
+				{
+					if (nameOccurrences.ContainsKey(key))
+					{
+						nameOccurrences[key] += 1;
+					}
+					else
+					{
+						nameOccurrences.Add(key, 1);
+					}
+				}
+			}
+		}
+		//build candidate list
 		int ccc = 0;
 		foreach (KeyValuePair<string, int> c in nameOccurrences.OrderByDescending(key => key.Value))
 		{
 			candidates.Add(new Candidate(c.Key, ccc++));
 		}
-		for (int i = 0; i <= intermediarySet.GetUpperBound(0); i++)
+		//get total poll listing - for map, this is summation of dateOccurrence values, for rollercoaster, length of dateOccurrence
+		int totalPolls = 0;
+		foreach (KeyValuePair<string, int> d in dateOccurrences.OrderByDescending(key => key.Value)) {
+			totalPolls += d.Value;
+		}
+		//build intermediaryMap
+		intermediaryMap = new string[totalPolls, cmx];
+		int iMCur = 0;
+		foreach (KeyValuePair<string, int> d in dateOccurrences.OrderByDescending(key => key.Value)) {	
+			Dictionary<string, List<int>> stateLookup = new Dictionary<string, List<int>>();
+			for (int i = 0; i <= intermediarySet.GetUpperBound(0); i++) {
+				string date = intermediarySet[i, 0];
+				string state = intermediarySet[i, 1];
+				if (date != null & state != null) {
+					if (date.Equals(d.Key)) {
+						if (stateLookup.ContainsKey(state)) {
+							stateLookup[state].Add(i);
+						}
+						else {
+							List<int> locs = new List<int>();
+							locs.Add(i);
+							stateLookup.Add(state, locs);
+						}
+					}
+				}
+			}
+			Dictionary<string, int> cands = new Dictionary<string, int>();
+			foreach (KeyValuePair<string, List<int>> s in stateLookup) {
+				int tVoters = 0;
+				foreach (int loc in s.Value) {
+					int voters = Int32.Parse(intermediarySet[loc, 2]);
+					tVoters += voters;
+					for (int j = 0; j < numAttributes-1; j++)
+					{
+						string cand = intermediarySet[loc, (j + j + 3)];
+						string percent = intermediarySet[loc, (j + j + 4)];
+						if (cand != null && percent != null && percent != "")
+						{
+							//print(percent);
+							int perc = Int32.Parse(percent); 
+							if (((perc * voters) / 100) >= voters) {
+								print("fuck");
+							}
+							if (cands.ContainsKey(cand))
+							{
+								cands[cand] += ((perc * voters) / 100);
+							}
+							else
+							{
+								cands.Add(cand, ((perc * voters) / 100));
+							}
+						}
+					}
+				}
+				intermediaryMap[iMCur, 0] = d.Key;
+				intermediaryMap[iMCur, 1] = s.Key;
+				intermediaryMap[iMCur, 2] = tVoters.ToString();
+				int nACur = 0;
+				foreach (KeyValuePair<string, int> c in cands.OrderByDescending(key => key.Value)) {
+					if (nACur < numAttributes-1) {
+						intermediaryMap[iMCur, (nACur + nACur + 3)] = c.Key;
+						intermediaryMap[iMCur, (nACur + nACur + 4)] = c.Value.ToString();
+						nACur++;
+					}
+				}
+				iMCur++;
+			}
+		}
+	}
+    static void buildCandidateStructure()
+    {
+		int numAttributes = (intermediaryMap.GetUpperBound(1) - 1) / 2;
+		for (int i = 0; i <= intermediaryMap.GetUpperBound(0); i++)
 	    {
 				//build poll container, key value pair with key date and value poll occurring on that date
 				Score[] scores = new Score[numAttributes];
 				for (int j = 0; j < numAttributes-1; j++) {
-					var candidate = candidates.FirstOrDefault(c => c.name == intermediarySet[i, (j+j+3)]);
-//				print("Num attributes: " + numAttributes);
-//				print("J: " + j + " J+J+4: " + (j+j+4));
-					string percent = intermediarySet[i, (j + j + 4)];
-					if (candidate != null && percent != null) {
-						//print (percent);
-						if (percent != "")
-							scores[j] = new Score(percent, candidate);
+					var candidate = candidates.FirstOrDefault(c => c.name == intermediaryMap[i, (j+j+3)]);
+					string v = intermediaryMap[i, 2];
+					string percent = intermediaryMap[i, (j + j + 4)];
+					if (candidate != null && percent != null && v != null) {
+						if (percent != "" && v != "") {
+							float voters = float.Parse(v);
+							float perc = float.Parse(percent);
+							//if (voters <= perc)
+								//print(intermediaryMap[i, 0] + " " + intermediaryMap[i,1] + " " + candidate.name + ": Votes: " + percent + " out of " + voters);
+							scores[j] = new Score(((perc/voters)*100).ToString(), candidate);
+						}
 					}
 				}
-				if (intermediarySet[i, 0] != null) {
-					Poll poll = new Poll(intermediarySet[i, 1],intermediarySet[i, 0],scores);
+				if (intermediaryMap[i, 0] != null) {
+					Poll poll = new Poll(intermediaryMap[i, 1],intermediaryMap[i, 0],scores);
 					//print (poll.date);
-					pollByDate.Add(poll.date, poll);
+					if (pollByDate.ContainsKey(poll.date)) {
+						pollByDate[poll.date].Add(poll);
+					} else {
+						List<Poll> list = new List<Poll>();
+						list.Add(poll);
+						pollByDate.Add(poll.date, list);
+					}
 				}
 		}
-				        
-        //go through and combine same date objects - state overlap? different poll, same state overlap?
-        foreach (KeyValuePair<string, int> date in dateOccurrences.OrderByDescending(key => key.Value))
-        {
-
-        }
-
-		//foreach (KeyValuePair<DateTime,Poll> poll in 
+				     
         //build candidate set, of format: all candidates sorted by occurence order, holding one poll object for each occurrence, sorted by datetime
         /*candidateSet = new PollObject[nameOccurrences.Count, (intermediarySet.GetUpperBound(0) + 1)];
         int candidateIterator = 0;
@@ -203,15 +281,26 @@ public class CSVReader : MonoBehaviour
             candidateIterator++;
         }*/
     }
-	public static void printPolls() {
-		foreach (KeyValuePair<DateTime, Poll> poll in pollByDate)
-		{
-			string msg = poll.Value.date + " " + poll.Value.state;
-			for (int i = 0; i < poll.Value.scores.Length; i++) {
-				if (poll.Value.scores[i] != null)
-					msg += " " + poll.Value.scores[i].candidate.name + " " + poll.Value.scores[i].percent;
+	public static void printIntermediaryMap() {
+		for (int i = 0; i <= intermediaryMap.GetUpperBound(0); i++) {
+			string msg = "";
+			for (int j = 0; j <= intermediaryMap.GetUpperBound(1); j++) {
+				msg += intermediaryMap[i, j] + " ";
 			}
 			print(msg);
+		}
+	}
+	public static void printPolls() {
+		foreach (KeyValuePair<DateTime, List<Poll>> pollList in pollByDate)
+		{
+			foreach (Poll poll in pollList.Value) {
+				string msg = poll.date + " " + poll.state;
+				for (int i = 0; i < poll.scores.Length; i++) {
+					if (poll.scores[i] != null)
+						msg += " " + poll.scores[i].candidate.name + " " + poll.scores[i].percent;
+				}
+				print(msg);
+			}
 		}
 	}
     public static void printCS()
