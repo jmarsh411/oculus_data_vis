9dae4d2fc32ac5518c276e0855385b796e0a1533
renketsu0 <joshmarshall21@gmail.com>
2015-11-10 18:24:43 -0500

    reformatted test Pins setup

--- Assets/scripts/TestPinNew.cs
+++ Assets/scripts/TestPinNew.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TestPinNew : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 		System.DateTime testDate = new System.DateTime (2012, 1, 27);
-		Poll testPoll = CSVReader.pollByDate [testDate];
-		GameObject pin2 = (GameObject)Instantiate(Pin.pinPrefab);
-		pin2.GetComponent<Pin>().Initialize(testPoll);
+		System.DateTime testDate2 = new System.DateTime (2012, 2, 20);
+		foreach (Poll poll in CSVReader.pollByDate [testDate]) {
+			GameObject pin = (GameObject)Instantiate (Pin.pinPrefab);
+			pin.GetComponent<Pin> ().Initialize (poll);
+		}
+		foreach (Poll poll in CSVReader.pollByDate [testDate2]) {
+			GameObject pin = (GameObject)Instantiate (Pin.pinPrefab);
+			pin.GetComponent<Pin> ().Initialize (poll);
+		}
 	}
 	
 	// Update is called once per frame
