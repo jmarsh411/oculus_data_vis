437fc246be7d378a787f2be89f3e9cd1419aef9f
renketsu0 <joshmarshall21@gmail.com>
2015-12-01 20:20:54 -0500

    billboard graph lines use unique colors

--- Assets/scripts/Make2DLine.cs
+++ Assets/scripts/Make2DLine.cs
@@ -3,26 +3,30 @@ using UnityEngine.UI.Extensions;
 using System.Collections;
 
 public class Make2DLine : MonoBehaviour {
-	CreateLine createLine;
 	GameObject cart;
 	UILineRenderer lineRenderer;
 
+	static Object material;
+	Material matInstance;
+
 	public void Initialize(Vector3[] points, string candidate){
+		// load material if not already loaded
+		if (material == null)
+			material = Resources.Load ("UILineRendererMaterial");
+
 		// setup references
 		cart = GameObject.Find("Cart");
-		createLine = cart.GetComponent<CreateLine>();
 		lineRenderer = GetComponent<UILineRenderer> ();
+
+		// create a new material instance and set its color
+		matInstance = new Material(material as Material);
+		matInstance.SetColor ("_Color", Candidate.candidateList [candidate].color);
+		lineRenderer.material = matInstance;
 
 		lineRenderer.Points = new Vector2[points.Length];
 		for (int i = 0; i < points.Length; i++) {
 			Vector3 vec3 = points [i];
 			lineRenderer.Points [i] = new Vector2 (vec3.z, vec3.y);
-			// color is changing, but it's not being visually updated
-			// neither of these is getting th ecolor update to occur.
-			lineRenderer.material.color = Candidate.candidateList[candidate].color;
-			lineRenderer.color = Candidate.candidateList[candidate].color;
-			// this forces an update (supposedly)
-			lineRenderer.SetAllDirty();
 		}
 	}
 
@@ -37,13 +41,5 @@ public class Make2DLine : MonoBehaviour {
 		rectTrans.offsetMax = Vector2.zero;
 		rectTrans.offsetMin = Vector2.zero;
 	}
-
-//	void Update() {
-//		// force update
-//		if (lineRenderer != null)
-//			lineRenderer.SetVerticesDirty ();
-//
-//	}
-
 
 }
