f1c4ff7864a07bad1df4d0810393ef04dd9add87
renketsu0 <joshmarshall21@gmail.com>
2015-12-02 21:58:19 -0500

    position marker for 2D billboard graph

---
+++ Assets/scripts/ProgressMarker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProgressMarker : MonoBehaviour {
+	RectTransform rectTrans;
+	GameObject cartRef;
+	Transform cartTrans;
+
+	// Use this for initialization
+	void Start () {
+		rectTrans = GetComponent<RectTransform> ();
+		cartRef = GameObject.Find ("Cart");
+		cartTrans = cartRef.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rectTrans.anchoredPosition = new Vector2(cartTrans.position.z, 0f);
+	}
+}
