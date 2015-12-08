b05d0f13ffb3cff022d3a94807ffbac3f942f7bf
justis <justisgill@gmail.com>
2015-12-07 23:25:40 -0500

    Full support for changing csv file
    All CSV files must match format specified in design document.
    Basic information: State must be first column, date must be third,
    number of voters must be eleventh, followed by candidate name then
    candidate percent, one pair for each candidate
    CSV files must be placed in Documents/VR Dashboard CSVs
    Example CSV also included here (2016DemPrimary.csv)

---
+++ Assets/scripts/ChangeCSV.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class ChangeCSV : MonoBehaviour {
+
+	
+	public Dictionary<string, string> filelist = new Dictionary<string, string> ();
+	bool ran = false;
+	int number = 0;
+	void getFiles() {
+		var info = new DirectoryInfo(System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments) + "\\VR Dashboard CSVs\\");
+		var files = info.GetFiles ("*.csv");
+		foreach (var file in files) {
+			filelist.Add(file.Name,file.FullName);
+			number++;
+		}
+	}
+
+	void OnGUI () {
+		if (!ran) {
+			getFiles ();
+			ran = true;
+		}
+		GUI.Box (new Rect (10, 10, 160, 70 + (20 * number)), "Choose CSV");
+
+		int one = 20, two = 40, three = 140, four = 20;
+		foreach (KeyValuePair<string,string> file in filelist) {
+			if (GUI.Button (new Rect (one, two, three, four), file.Key)) {
+				CSVReader.parse(file.Value);
+			}
+			two += 30;
+		}
+	}
+}
--- Assets/scripts/Poll.cs
+++ Assets/scripts/Poll.cs
@@ -13,7 +13,17 @@ public class Poll {
 	}
 	public Poll(string s, string d, Score[] sc) {
 		state = s;
-		date = DateTime.ParseExact(d, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+		try {
+			date = DateTime.ParseExact(d, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+		}
+		catch (Exception e) {
+			try {
+				date = DateTime.ParseExact(d, "MM/dd/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+			}
+			catch (Exception ex) {
+				date = new DateTime(2001,01,01);
+			}
+		}
 		scores = sc;
 	}
 }
