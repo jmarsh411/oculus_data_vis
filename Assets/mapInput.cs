3880abe26dc23f10217bb5b87e9a20f308ebb803
tvasher <dw9110@wayne.edu>
2015-12-13 15:32:31 -0500

    Fixed Parentheses Mistake

--- Assets/mapInput.cs
+++ Assets/mapInput.cs
@@ -7,7 +7,7 @@ public class mapInput : MonoBehaviour {
 	
 	void Update() {
 	
-			if (Input.GetKey(KeyCode.Escape)) || Input.GetKey("joystick 1 button 8")) //Needs to be mapped to JoystickButton
+			if (Input.GetKey(KeyCode.Escape) || Input.GetKey("joystick 1 button 8")) //Needs to be mapped to JoystickButton
 			{
 						
 				Application.LoadLevel(0); // Goes Back to Menu
