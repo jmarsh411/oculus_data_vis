d2b44f45b3c9f8862402f4a7bd8bbd89eed32191
renketsu0 <joshmarshall21@gmail.com>
2015-12-13 16:28:17 -0500

    added UITimeline script to control timelines

---
+++ Assets/scripts/UITimeline.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UITimeline : MonoBehaviour {
+	private GameObject contextRect;
+	private float userY;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		matchUserHeight ();
+	}
+
+	void matchUserHeight() {
+		transform.position = new Vector3(transform.position.x,
+		                                 userY,
+		                                 transform.position.z);
+	}
+
+}
