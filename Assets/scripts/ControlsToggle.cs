ba3e0049a5f662be1253767d5eba214d5ee8a453
renketsu0 <joshmarshall21@gmail.com>
2015-12-14 01:07:30 -0500

    added feature to toggle controls overlay

---
+++ Assets/scripts/ControlsToggle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlsToggle : MonoBehaviour {
+	CanvasGroup canvasGroup;
+
+	// Use this for initialization
+	void Start () {
+		canvasGroup = GetComponent<CanvasGroup> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("joystick 1 button 3")) {
+			if (canvasGroup.alpha == 1)
+				canvasGroup.alpha = 0;
+			else
+				canvasGroup.alpha = 1;
+			}
+	}
+}
