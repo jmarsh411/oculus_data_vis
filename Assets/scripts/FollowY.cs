1d3f6c94c7114b9d4043162b29c5e931549e0563
renketsu0 <joshmarshall21@gmail.com>
2015-12-13 20:55:59 -0500

    updated UItimeline and context prefabs

---
+++ Assets/scripts/FollowY.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowY : MonoBehaviour {
+	private GameObject contextPanel;
+	private Transform cartTrans;
+
+	// Use this for initialization
+	void Start () {
+		// get cart transform reference
+		GameObject cart = GameObject.Find("Cart");
+		cartTrans = cart.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		matchUserHeight ();
+	}
+
+	void matchUserHeight() {
+		transform.position = new Vector3(transform.position.x,
+		                                 cartTrans.position.y,
+		                                 transform.position.z);
+	}
+}
