0f4d3227029c4be50d69a5d82008c35da4d444cb
renketsu0 <joshmarshall21@gmail.com>
2015-12-17 18:13:02 -0500

    added method to get main camera for canvases

---
+++ Assets/scripts/CanvasGetMainCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CanvasGetMainCamera : MonoBehaviour {
+
+	void Awake() {
+		// use the main camera for this Canvas
+		GetComponent<Canvas> ().worldCamera = Camera.main;
+	}
+}
